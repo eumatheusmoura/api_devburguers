326425b7fb945bd2884b3b0a17366aa14e68d298
Matheus Moura <matheusmouraa@icloud.com>
2022-09-06 21:50:37 -0300

    feat: inserindo atributo de string na entidade

--- src/model/pedidos.js
+++ src/model/pedidos.js
@@ -1,13 +1,14 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-const Pedidos = mongoose.model('pedidos', {
-    pedido: String,
-    nome: String,
-    endereco: String,
-    itensdopedido: String,
-    formadepagamento: String,
-    mododeentrega: String,
-    total: String
-})
+const Pedidos = mongoose.model("pedidos", {
+  img: String,
+  pedido: String,
+  nome: String,
+  endereco: String,
+  itensdopedido: String,
+  formadepagamento: String,
+  mododeentrega: String,
+  total: String,
+});
 
-export default Pedidos
+export default Pedidos;
